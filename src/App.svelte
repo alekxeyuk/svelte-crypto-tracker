344ce2b55b15af2df80a481836258e86fd07c947
LeLuCh <alekx@alumni.nottingham.ac.uk>
2021-06-11 23:05:11 +0800

    Extract Update component

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
     import { onMount } from 'svelte';
     import CoinCard from "./lib/CoinCard.svelte";
     import Loader from './lib/Loader.svelte';
+    import UpdateButton from './lib/UpdateButton.svelte';
 
     let name = "Crypto Tracker";
 
@@ -25,7 +26,7 @@
     {#if coins.length === 0}
         <Loader/>
     {:else}
-        <button on:click={fetchCoins} class="myButton">Update Coin Data!</button>
+        <UpdateButton {fetchCoins}/>
         <div class="grid">
             {#each coins as coin}
                 <CoinCard {coin} />
@@ -73,30 +74,5 @@
             max-width: 1600px;
             padding: 40px 20px;
         }
-    }
-
-    .myButton {
-        box-shadow: inset 0px 1px 0px 0px #ffffff;
-        background: linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%);
-        background-color: #ffffff;
-        border-radius: 6px;
-        border: 1px solid #dcdcdc;
-        display: inline-block;
-        cursor: pointer;
-        color: #666666;
-        font-family: Arial;
-        font-size: 15px;
-        font-weight: bold;
-        padding: 6px 24px;
-        text-decoration: none;
-        text-shadow: 0px 1px 0px #ffffff;
-    }
-    .myButton:hover {
-        background: linear-gradient(to bottom, #f6f6f6 5%, #ffffff 100%);
-        background-color: #f6f6f6;
-    }
-    .myButton:active {
-        position: relative;
-        top: 1px;
     }
 </style>
---
+++ src/lib/UpdateButton.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let fetchCoins;
+</script>
+
+<button on:click={fetchCoins} class="myButton">Update Coin Data!</button>
+
+<style>
+    .myButton {
+        box-shadow: inset 0px 1px 0px 0px #ffffff;
+        background: linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%);
+        background-color: #ffffff;
+        border-radius: 6px;
+        border: 1px solid #dcdcdc;
+        display: inline-block;
+        cursor: pointer;
+        color: #666666;
+        font-family: Arial;
+        font-size: 15px;
+        font-weight: bold;
+        padding: 6px 24px;
+        text-decoration: none;
+        text-shadow: 0px 1px 0px #ffffff;
+    }
+    .myButton:hover {
+        background: linear-gradient(to bottom, #f6f6f6 5%, #ffffff 100%);
+        background-color: #f6f6f6;
+    }
+    .myButton:active {
+        position: relative;
+        top: 1px;
+    }
+</style>
